06e80b2ac6e495c0dbf2e966c931b405d8996314
Maria Komendantova <masha.komendantova@yandex.ru>
2022-10-27 14:15:07 +0300

    update checkstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,15 +8,15 @@ import hexlet.code.games.Prime;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Please enter the game number and press Enter.\n" +
-                "1 - Greet\n" +
-                "2 - Even\n" +
-                "3 - Calc\n" +
-                "4 - GCD\n" +
-                "5 - Progression\n" +
-                "6 - Prime\n" +
-                "0 - Exit\n" +
-                "Your choice: ");
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calc\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
         Scanner sc = new Scanner(System.in);
         int game = sc.nextInt();
         switch (game) {
